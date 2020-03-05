84dacf3d9865a74974f84c35b15861ad7e061aa2
Jonghun Park <jonghunpark@ucsd.edu>
2020-03-04 23:16:42 -0800

    Add a function getting Jaccard Index automatically from the distribution

--- code/graph_constructor.py
+++ code/graph_constructor.py
@@ -1,9 +1,53 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy
+import numpy as np
+
+
+def get_jaccard_index_threshold_from_kde(input_file, target_percentile=0.97):
+    data_folder = "../data/"
+    matrix_file = data_folder + input_file
+    mutation_dict = dict()
+    with open(matrix_file, "r") as f:
+        header = f.readline()
+        for line in f:
+            split = line.strip().split(",")
+            mutation_type = split[0]
+            trinucleotide = split[1]
+            mutation_dict[mutation_type + "/" + trinucleotide] = split[2:]
+
+    jaccard_index_list = []
+    for mutation_type1, list1 in mutation_dict.items():
+        for mutation_type2, list2 in mutation_dict.items():
+            set1 = set([j for j in range(len(list1)) if list1[j] == '1'])
+            set2 = set([j for j in range(len(list2)) if list2[j] == '1'])
+            jaccard_index_list.append(compute_jaccard_index(set1, set2))
+
+    plt.title("Jaccard Index Distribution of {} Sample".format(input_file.split(".")[0]))
+    plt.xlabel("Jaccard Index")
+    plt.ylabel("Density")
+
+    p = sns.kdeplot(jaccard_index_list, cumulative=False, shade=True)
+    xmin, xmax, ymin, ymax = plt.axis()
+    x, y = p.get_lines()[0].get_data()
+    print(x, y)
+    # care with the order, it is first y
+    # initial fills a 0 so the result has same length than x
+    cdf = scipy.integrate.cumtrapz(y, x, initial=0)
+    target_index = np.abs(cdf - target_percentile).argmin()
+
+    x_point = x[target_index]
+    y_point = y[target_index]
+    plt.vlines(x_point, 0, ymax, colors="b", linestyles='--')
+    # plt.show()
+
+    return x_point
 
 
 def compute_jaccard_index(set1, set2):
     intersect = set1 & set2
-    jaccard_index = float(len(intersect)) / (len(set1) + len(set2))
+    jaccard_index = float(len(intersect)) / len(set1.union(set2))
     return jaccard_index
 
 
@@ -64,6 +108,13 @@ def output_graph(graph, outfile_name="graph_edge_node.csv"):
 if __name__ == "__main__":
     jaccard_index_threshold = 0.4
     matrix_file = "Skin-Melanoma.csv"
+    # matrix_file = "Pheochromocytoma.csv"
+    # matrix_file = "Cervix-CA.csv"
+    # matrix_file = "Transitional-cell-carcinoma.csv"
+    # matrix_file = "Eso-AdenoCa.csv"
+    # matrix_file = "ColoRect-AdenoCa.csv"
+
+    jaccard_index_threshold = get_jaccard_index_threshold_from_kde(matrix_file)
     graph = construct_graph(matrix_file, jaccard_index_threshold)
     # output_graph(graph)
     draw_graph(graph, jaccard_index_threshold)
