61acf8089aceb0b9743c7090fd610898cf1a282a
Jonghun Park <jonghunpark@ucsd.edu>
2020-03-05 15:54:15 -0800

    Add weight on graph

--- code/graph_constructor.py
+++ code/graph_constructor.py
@@ -24,6 +24,7 @@ def get_jaccard_index_threshold_from_kde(input_file, target_percentile=0.97):
             set2 = set([j for j in range(len(list2)) if list2[j] == '1'])
             jaccard_index_list.append(compute_jaccard_index(set1, set2))
 
+    plt.figure()
     plt.title("Jaccard Index Distribution of {} Sample".format(input_file.split(".")[0]))
     plt.xlabel("Jaccard Index")
     plt.ylabel("Density")
@@ -31,7 +32,7 @@ def get_jaccard_index_threshold_from_kde(input_file, target_percentile=0.97):
     p = sns.kdeplot(jaccard_index_list, cumulative=False, shade=True)
     xmin, xmax, ymin, ymax = plt.axis()
     x, y = p.get_lines()[0].get_data()
-    print(x, y)
+    # print(x, y)
     # care with the order, it is first y
     # initial fills a 0 so the result has same length than x
     cdf = scipy.integrate.cumtrapz(y, x, initial=0)
@@ -45,15 +46,13 @@ def get_jaccard_index_threshold_from_kde(input_file, target_percentile=0.97):
     return x_point
 
 
-
 def compute_jaccard_index(set1, set2):
     intersect = set1 & set2
-    jaccard_index = float(len(intersect)) / (len(set1) + len(set2))
     jaccard_index = float(len(intersect)) / len(set1.union(set2))
     return jaccard_index
 
 
-def construct_graph(input_file, jaccard_index_threshold=0.3):
+def construct_graph(input_file, jaccard_index_threshold=0.3, weighted=True, out_graph=False):
     # Loading Matrix file
     data_folder = "../data/"
     matrix_file = data_folder + input_file
@@ -76,35 +75,58 @@ def construct_graph(input_file, jaccard_index_threshold=0.3):
             set2 = set([j for j in range(len(list2)) if list2[j] == '1'])
             jaccard_index = compute_jaccard_index(set1, set2)
             if jaccard_index > jaccard_index_threshold:
-                G.add_edge(mutation_type1, mutation_type2)
+                if weighted:
+                    G.add_edge(mutation_type1, mutation_type2, weight=jaccard_index)
+                else:
+                    G.add_edge(mutation_type1, mutation_type2)
 
     # Output graph as adjacent list
-    outfile = data_folder + matrix_file.split("/")[-1].split(".")[0] + "_graph_" + str(jaccard_index_threshold) + ".csv"
-    with open(outfile, "w") as f:
-        for node in G.nodes():
-            f.write(node)
-            f.write(":")  # adjacent list delimiter
-            f.write(','.join(G[node].keys()))
-            f.write("\n")
+    if out_graph:
+        outfile = data_folder + matrix_file.split("/")[-1].split(".")[0] + "_graph_" + str(jaccard_index_threshold) + ".csv"
+        with open(outfile, "w") as f:
+            for node in G.nodes():
+                f.write(node)
+                f.write(":")  # adjacent list delimiter
+                f.write(','.join(G[node].keys()))
+                f.write("\n")
 
     return G
 
 
-def draw_graph(G, jaccard_index_threshold):
+def draw_graph(G, jaccard_index_threshold=None):
     import matplotlib.pyplot as plt
+    plt.figure()
     nx.draw_networkx(G)
+    # pos = nx.spring_layout(G)
+    # nx.draw_networkx_nodes(G, pos, node_color='b')
+    # nx.draw_networkx_edges(G, pos, width=1.0)
+    if jaccard_index_threshold is not None:
+        plt.title("Jaccard index threshold {}".format(jaccard_index_threshold))
     plt.savefig("../figures/" + "graph_jaccard_index_" + str(jaccard_index_threshold) + ".png")
+
+
+def output_graph(graph, outfile_name="graph_edge_node.csv"):
+    with open(outfile_name, "w") as f:
+        for edge in graph.edges_iter():
+            f.write(str(edge[0]) + "," + str(edge[1]) + "\n")
+            if edge[0] != edge[1]:
+                f.write(str(edge[0]) + "," + str(edge[1]) + "\n")
+
+        for node in graph.nodes_iter():
+            f.write(str(node) + "\n")
 
 
 if __name__ == "__main__":
     jaccard_index_threshold = 0.4
-    matrix_file = "Skin-Melanoma_ignore_0.csv"
+    matrix_file = "Lung-SCC.csv"
+    # matrix_file = "Skin-Melanoma.csv"
     # matrix_file = "Pheochromocytoma.csv"
     # matrix_file = "Cervix-CA.csv"
     # matrix_file = "Transitional-cell-carcinoma.csv"
     # matrix_file = "Eso-AdenoCa.csv"
     # matrix_file = "ColoRect-AdenoCa.csv"
 
-    jaccard_index_threshold = get_jaccard_index_threshold_from_kde(matrix_file)
-    graph = construct_graph(matrix_file, jaccard_index_threshold)
-    draw_graph(graph, jaccard_index_threshold)
+    jaccard_index_threshold = get_jaccard_index_threshold_from_kde(matrix_file, 0.90)
+    graph = construct_graph(matrix_file, jaccard_index_threshold, weighted=True)
+    print([(u, v, edata['weight']) for u, v, edata in graph.edges(data=True) if 'weight' in edata])
+    # draw_graph(graph, jaccard_index_threshold)
